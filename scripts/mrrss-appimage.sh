ef287454033a5e0faac31d558505cd25b693ee1a
a15355447898a <a15355447898a@outlook.com>
2026-09-07 11:21:38 +0800

    更新一个软件包

--- scripts/mrrss-appimage.sh
+++ scripts/mrrss-appimage.sh
@@ -1,5 +1,5 @@
 cd mrrss-appimage
-latest_ver=$(curl -s https://api.github.com/repos/WCY-dt/MrRSS/releases/latest | jq -r .tag_name | tr -d 'v')
+latest_ver=$(curl -s https://api.github.com/repos/DevXDojo/MrRSS/releases/latest | jq -r .tag_name | tr -d 'v')
 sed -i "s/pkgver=.*/pkgver=$latest_ver/" PKGBUILD
 sudo -u builder updpkgsums
 sudo -u builder makepkg --printsrcinfo > .SRCINFO
